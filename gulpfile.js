a110867ff55fadcdbada5a087004a61425763e12
Fabrício Oliveira <fabloliv@outlook.com>
2017-09-23 03:05:02 -0300

    Starting gulp and sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('source-files')
+    return gulp.src('assets/scss/**/*.scss')
         .pipe(sass()) // Using gulp-sass
-        .pipe(gulp.dest('destination'))
+        .pipe(gulp.dest('assets/css'))
 });
+
+// Gulp watch syntax
+gulp.task('watch', function() {
+    gulp.watch('assets/scss/**/*.scss', ['sass']);
+    // Other watchers
+})
