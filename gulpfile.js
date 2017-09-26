9b7006a6f5e6cd43670a5f9c77074c6a9849514d
Fabrício Oliveira <fabloliv@outlook.com>
2017-09-25 22:36:20 -0300

    Setup gulp-sass and testing BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-    return gulp.src('assets/scss/**/*.scss')
-        .pipe(sass()) // Using gulp-sass
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('assets/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
 // Gulp watch syntax
-gulp.task('watch', function() {
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
-    // Other watchers
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('./*.html', browserSync.reload);
+    //gulp.watch('assets/js/**/*.js', browserSync.reload);
+})
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        },
+    })
 })
