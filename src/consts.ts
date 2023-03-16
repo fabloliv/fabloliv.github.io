8e0e56690728f5d1983f9f6e1c6bc2c8d6ce0d3f
Fabrício Oliveira <fabriciololiveira@gmail.com>
2023-03-16 07:20:57 -0300

    Edit global data

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Fabrício Oliveira | Desenvolvedor Front-End";
+export const SITE_DESCRIPTION = "Sou Desenvolvedor Front-end em São Pedro da Aldeia-RJ. Construo sites para pequenas empresas, com foco em performance, usabilidade e SEO.";
