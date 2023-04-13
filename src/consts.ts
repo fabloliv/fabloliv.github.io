9c83f218961e1593980b00f4c8ca5dd29a174e05
Fabrício Oliveira <fabriciololiveira@gmail.com>
2023-04-13 04:47:43 -0300

    chore(consts.ts): file moved

--- src/consts.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Fabrício Oliveira | Desenvolvedor Front-End";
-export const BLOG_PAGE_TITLE = "Blog | Fabrício Oliveira - Desenvolvedor Front-End";
-export const BLOG_POST_TITLE = " | Blog | Fabrício Oliveira";
-export const SITE_DESCRIPTION = "Sou Desenvolvedor Front-end em São Pedro da Aldeia-RJ. Construo sites para pequenas empresas, com foco em performance, usabilidade e SEO.";
