ec6d347dacc319d279e79f6756c4a70dd7b0cd55
Fabrício Oliveira <fabriciololiveira@gmail.com>
2023-04-13 03:39:10 -0300

    feat(astro.config): add and configure astro-compress package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fabloliv.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), compress()],
   markdown: {
     syntaxHighlight: "prism",
   },
